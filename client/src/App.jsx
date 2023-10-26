1b17f0c939b71d9b77cdb51803e807289acf5ee4
Ahmed-Taimoor <malikahmadtamoor@gmail.com>
2023-10-26 06:17:31 +0500

    s

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
 function App() {
   return (
-    <>
-      <h1 className="head-text">Three Js</h1>
-    </>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,21 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from '../config/motion';
+import state from '../store';
 import React from 'react';
 
 const Home = () => {
-  return <div></div>;
+  const snap = useSnapshot(state);
+  return (
+    <>
+      <AnimatePresence></AnimatePresence>
+    </>
+  );
 };
 
 export default Home;
